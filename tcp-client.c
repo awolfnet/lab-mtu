02bc1ae0e8bbad7dcb33a093b999553a09074d89
wolf <wolf@server.lan01>
2021-06-22 08:57:57 -0400

    remove md5 add send header

--- tcp-client.c
+++ tcp-client.c
@@ -8,8 +8,9 @@
 
 #define CONNECT_PORT 10000
 #define LENGTH 6000
+#define SEND_TIMES 5
 
-#define DEFAULT_TAG 0x07
+#define DEFAULT_TAG 0x21
 
 typedef unsigned char BYTE;
 
@@ -20,13 +21,12 @@ typedef struct
     int data_length;
     uLong data_checksum;
     uLong header_checksum;
-    BYTE *data
-} MYPACKET;
+} HEADER;
 
 int main(int argc, char *argv[])
 {
     char *address = NULL;
-    if (4 == argc && argv[1] != NULL)
+    if (5 == argc && argv[1] != NULL)
     {
         address = argv[1];
     }
@@ -36,15 +36,21 @@ int main(int argc, char *argv[])
     }
 
     int port = CONNECT_PORT;
-    if (4 == argc && argv[2] != NULL)
+    if (5 == argc && argv[2] != NULL)
     {
         port = atoi(argv[2]);
     }
 
     int length = LENGTH;
-    if (4 == argc && argv[3] != NULL)
+    if (5 == argc && argv[3] != NULL)
     {
         length = atoi(argv[3]);
+    }
+
+    int times = SEND_TIMES;
+    if (5 == argc && argv[4] != NULL)
+    {
+        times = atoi(argv[4]);
     }
 
     printf("Connect to %s:%d, send data length %d\r\n", address, port, length);
@@ -77,17 +83,47 @@ int main(int argc, char *argv[])
     printf("Connected.\r\n");
 
     BYTE *buffer = calloc(length, sizeof(BYTE));
-    memset((void *)buffer, 'A', length - 1);
-
-
-
-    if (send(socket_client_fd, buffer, length, 0) < 0)
+    while (times-- > 0)
     {
-        printf("send error(%d): %s\r\n", errno, strerror(errno));
-        close(socket_client_fd);
-        free(buffer);
-        return EXIT_FAILURE;
+        memset((void *)buffer, 'A' + times, length - 1);
+        uLong buffer_crc = crc32(0L, buffer, length);
+
+        HEADER header;
+        memset((void *)&header, 0x00, sizeof(header));
+
+        //TAg
+        header.tag = DEFAULT_TAG;
+
+        //SYN
+        sprintf((char *)header.syn, "%x", times);
+
+        //Data length
+        header.data_length = length;
+
+        //Data checksum
+        header.data_checksum = buffer_crc;
+
+        //Header checksum
+        header.header_checksum = 0L;
+        uLong header_crc = crc32(0L, (const Bytef *)&header, sizeof(header));
+        header.header_checksum = header_crc;
+
+        int header_length = sizeof(header);
+        printf("Sendig %d\r\n", times);
+        printf("Sending header, length(%d), syn(%x), datalength(%u), datachecksum(%u), headerchecksum(%u)\r\n", header_length, *header.syn, header.data_length, header.data_checksum, header.header_checksum);
+        if (send(socket_client_fd, &header, header_length, 0) < 0)
+        {
+            printf("send header error(%d): %s\r\n", errno, strerror(errno));
+            break;
+        }
+        printf("Sending data, length(%d)\r\n", length);
+        if (send(socket_client_fd, buffer, length, 0) < 0)
+        {
+            printf("send data error(%d): %s\r\n", errno, strerror(errno));
+            break;
+        }
     }
+
     close(socket_client_fd);
     free(buffer);
     return EXIT_SUCCESS;
